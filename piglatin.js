ddd30cdb4d262ff8893762bef7a39701484ab89e
pemtshewang <pemtshewang500@gmail.com>
2023-03-04 11:38:11 +0600

    updated piglatin.js

--- piglatin.js
+++ piglatin.js
@@ -1,6 +1,16 @@
 function translatePigLatin(str) {
-  return /[aeiou]/.test(str[0]) ? str+"way" : str.slice(1)+str[0]+"ay";
+  if(/[aeiou]/.test(str[0])){
+    return str+"way";
+ }
+  if(/[aeiou]/.test(str)){
+    for(let i=0; i<str.length; i++){
+      if(/[aeiou]/.test(str[i])){
+        return str.slice(i)+str.slice(0,i)+"ay";
+      }
+    }
+  }
+  return str+"ay";
 }
 
 console.log(translatePigLatin("algorithm"))
-console.log(translatePigLatin("cat"))
+console.log(translatePigLatin("rhythm"))
